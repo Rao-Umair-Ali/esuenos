4aad8ab17025d7c9eed5eb01bcdabacf84b7e46a
Rao-Umair-Ali <ruarua682@gmail.com>
2024-09-25 15:27:28 +0500

    mainscreen

--- lib/login_page/loginScreen.dart
+++ lib/login_page/loginScreen.dart
@@ -14,24 +14,33 @@ class Login extends StatelessWidget {
           image: DecorationImage(
               image: AssetImage("assets/images/Group.png"), fit: BoxFit.cover)),
       child: Center(
-        child: Container(
-          width: 80, // Width of the circle
-          height: 80, // Height of the circle (same as width for roundness)
-          decoration: BoxDecoration(
-            color: Color(0xff4157FF), // Background color
-            shape: BoxShape.circle, // Circular shape
-            border: Border.all(
-              color: Colors.white, // Border color
-              width: 4, // Border thickness
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 80, // Width of the circle
+              height: 80, // Height of the circle (same as width for roundness)
+              decoration: BoxDecoration(
+                color: const Color(0xff4157FF), // Background color
+                shape: BoxShape.circle, // Circular shape
+                border: Border.all(
+                  color: Colors.white, // Border color
+                  width: 4, // Border thickness
+                ),
+              ),
+              child: const Center(
+                child: Icon(
+                  Icons.local_hospital_rounded,
+                  color: Colors.white,
+                  size: 60, // Adjust the size of the icon
+                ),
+              ),
             ),
-          ),
-          child: const Center(
-            child: Icon(
-              Icons.local_hospital_rounded,
-              color: Colors.white,
-              size: 60, // Adjust the size of the icon
-            ),
-          ),
+            const Text(
+              "Quick Medical",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            )
+          ],
         ),
       ),
     ));
