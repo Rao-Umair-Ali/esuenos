529db199cf094516b0a811c574ec5af1811c1e82
Rao-Umair-Ali <ruarua682@gmail.com>
2024-09-25 15:19:16 +0500

    .

---
+++ lib/login_page/loginScreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      height: 1000,
+      width: 1000,
+      decoration: const BoxDecoration(
+          color: Color(0xff4157FF),
+          image: DecorationImage(
+              image: AssetImage("assets/images/Group.png"), fit: BoxFit.cover)),
+      child: Center(
+        child: Container(
+          width: 80, // Width of the circle
+          height: 80, // Height of the circle (same as width for roundness)
+          decoration: BoxDecoration(
+            color: Color(0xff4157FF), // Background color
+            shape: BoxShape.circle, // Circular shape
+            border: Border.all(
+              color: Colors.white, // Border color
+              width: 4, // Border thickness
+            ),
+          ),
+          child: const Center(
+            child: Icon(
+              Icons.local_hospital_rounded,
+              color: Colors.white,
+              size: 60, // Adjust the size of the icon
+            ),
+          ),
+        ),
+      ),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
-main() {}
+import 'package:esuenos/login_page/loginScreen.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    );
+  }
+}
